f47a442322d132f847fa0dcd3f75168462874a15
soossaeed246 <soossaeed246@gmail.com>
2026-07-14 09:06:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // وقت فتح التحقق
-const openTime = new Date("2026-08-16T08:01:00+03:00");
+const openTime = new Date("2026-08-16T15:00:00+03:00");
 
 // وقت إغلاق التحقق
 const closeTime = new Date("2026-08-18T04:00:00+03:00");
